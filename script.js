29550d60777b2cc3e7acca8563e567a358755d99
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-07-05 17:22:11 +0530

    input form values

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
+let countdownTitle = '';
+let countdownDate = '';
 
 // Set date input min with today date
-const today=new Date().toISOString().split('T')[0];
-dateEl.setAttribute('min',today);
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+// take values from form input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// event listener
+countdownForm.addEventListener('submit', updateCountdown);
